f5a88a6d6efbc24c432079599aaf35a466eea890
Eduardo <eduardomedranop@gmail.com>
2024-12-02 23:13:54 -0300

    agregando logica Favs para que cuando no alla favoritos

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -4,15 +4,15 @@ import Card from "../Components/Card";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
- const [favs, setFavs] = useState([]);
+  const [favs, setFavs] = useState([]);
 
- useEffect(() => {
-  const obtenerFavoritos = () => {
-    const favoritosGuardados = localStorage.getItem("Favoritos");
-    return favoritosGuardados ? JSON.parse(favoritosGuardados) : [];
-  };
-  setFavs(obtenerFavoritos());
- }, [])
+  useEffect(() => {
+    const obtenerFavoritos = () => {
+      const favoritosGuardados = localStorage.getItem("Favoritos");
+      return favoritosGuardados ? JSON.parse(favoritosGuardados) : [];
+    };
+    setFavs(obtenerFavoritos());
+  }, [])
 
 
 
@@ -20,9 +20,17 @@ const Favs = () => {
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-       {favs.map((e)=>{
-         return  <Card key={e.id} {...e} />
-       })}
+        {favs.length > 0 ? ( // Use parentheses here
+
+          favs.map((e) => (
+            <Card key={e.id} {...e} />
+          ))
+
+        ) : (
+
+          <h2>No hay favoritos</h2>
+
+        )}
       </div>
     </>
   );
